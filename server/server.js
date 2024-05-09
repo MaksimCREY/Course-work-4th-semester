3b2336b445487df7ca0f6c9561f677a5273f5a77
MaksimCREY <maksimctrey@gmail.com>
2024-05-09 21:42:23 +0300

    v1

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mssql = require('mssql');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5500;
 
 // Подключение к базе данных
